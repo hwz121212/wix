c5047ca0e6bc15dbe9d3a2b53d795c9604237843
hanwenzhi <702054083@qq.com>
2022-10-27 10:13:48 +0800

    官网：https://wixtoolset.org/
    1. backup目录下的wix314-debug.zip是从https://wixtoolset.org/releases/development/下载而来的。下载时间【北京时间2022-08-06】
    2. wix-3.14目录下的代码是由backup目录下的wix314-debug.zip重构而来的，重构时间【北京时间2022-08-06】

---
+++ wix-3.14/thirdparty/wix-3.14/src/balutil/precomp.h
@@ -0,0 +1,25 @@
+#pragma once
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+
+#include <windows.h>
+#include <bitsmsg.h>
+#include <msi.h>
+#include <wininet.h>
+
+#include <dutil.h>
+#include <pathutil.h>
+#include <locutil.h>
+#include <memutil.h>
+#include <strutil.h>
+#include <xmlutil.h>
+
+#include "IBootstrapperEngine.h"
+#include "IBootstrapperApplication.h"
+
+#include "IBootstrapperBAFunction.h"
+
+#include "balutil.h"
+#include "balcondition.h"
+#include "balinfo.h"
+#include "balretry.h"
